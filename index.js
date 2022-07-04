391737371f4ec7dcbaf050c76e6129bbd84023bc
David Whitney <codifyamplify@gmail.com>
2022-07-04 16:20:05 -0400

    commit tree dfd1262c82a6a939f93b065d40ebfe6cc9a4546c

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record){
+    for(const element of record){
+        if (element.result === "W"){
+            return element.year
+        }
+    }
+}
